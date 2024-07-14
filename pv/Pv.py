d92518ebc1bc40625c68e2b61e228333848b0293
rowesrw <117420517+rowesrw@users.noreply.github.com>
2024-07-14 14:40:58 +0300

    Update Pv.py

--- pv/Pv.py
+++ pv/Pv.py
@@ -1,1 +1,57 @@
+from pyrogram import Client, filters, idle
+from config import OWNER
+from source.info import (get_users, add_new_user)
+from source.Data import (get_dev)
+from pyrogram.types import Message
 
+
+OFFPV = []
+
+@Client.on_message(filters.command(["• تفعيل التواصل •","• تعطيل التواصل •"], ""))
+async def byyye(client, message):
+    user = message.from_user.username
+    dev = await get_dev(client.me.username)
+    if user in OWNER or message.from_user.id == dev:
+        text = message.text
+        if text == "• تفعيل التواصل •"":
+          if not client.me.username in OFFPV:
+             await message.reply_text("**التواصل مفعل من قبل .**")
+          try:
+            OFFPV.remove(client.me.username)
+            await message.reply_text("**تم تفعيل التواصل .**")
+            return
+          except:
+             pass
+        if text == "• تعطيل التواصل •":
+          if client.me.username in OFFPV:
+             await message.reply_text("**التواصل معطل من قبل .**")
+          try:
+            OFFPV.append(client.me.username)
+            await message.reply_text("**تم تعطيل التواصل .**")
+            return
+          except:
+             pass
+
+
+@Client.on_message(filters.private)
+async def botoot(client: Client, message: Message):
+
+  bot_username = client.me.username
+  user_id = message.chat.id
+  if not await get_users(client, user_id):
+     await add_new_user(client, user_id)
+  dev = await get_dev(bot_username)
+  if message.from_user.id == dev or message.chat.username in OWNER or message.from_user.id == client.me.id:
+    if message.reply_to_message:
+     u = message.reply_to_message.forward_from
+     try:
+       await client.send_message(u.id, text=message.text)
+       await message.reply_text(f"**تم ارسال رساتلك إلي {u.mention} بنجاح .**")
+     except Exception:
+         pass
+  else:
+   try:
+    await client.forward_messages(dev, message.chat.id, message.id)
+    await client.forward_messages(OWNER[0], message.chat.id, message.id)
+   except Exception as e:
+     pass
