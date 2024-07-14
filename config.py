ff793ecbb9b710e0f84c858c6ac195e7e9260a49
rowesrw <117420517+rowesrw@users.noreply.github.com>
2024-07-14 14:02:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ db = asJSON("as.json")
 ###
 
 
-SUDORS = [5541009328] # ايديات المطورين
+SUDORS = [5749137933] # ايديات المطورين
 API_ID = 9028013
 API_HASH = "cc894fc40424f9c8bbcf06b7355bd69d"
-TOKEN = "" # التوكن
+TOKEN = "6484265015:AAHG02-xH7G5Ccrlt_hzy7ajjnHF9CIJ4Do" # التوكن
 bot = Client("control",API_ID,API_HASH,bot_token=TOKEN,in_memory=True)
 bot_id = TOKEN.split(":")[0]
